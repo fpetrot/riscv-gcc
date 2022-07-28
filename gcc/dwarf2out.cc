8f46a9dd965fb28c3b13cf1050a2345b4124eb18
Sylvain Noiry <sylvain.noiry@grenoble-inp.org>
2022-07-28 10:42:22 +0200

    rv128: start support of 128-bit target in dwarf2
    support 16-bytes data handling
    
    gcc/ChangeLog:
    
    	* gcc/dwarf2out.cc: add cases for 128-bit target handling (RISC-V rv128)

--- gcc/dwarf2out.cc
+++ gcc/dwarf2out.cc
@@ -297,8 +297,8 @@ static GTY(()) bool do_eh_frame = false;
 static unsigned int rnglist_idx;
 
 /* Data and reference forms for relocatable data.  */
-#define DW_FORM_data (dwarf_offset_size == 8 ? DW_FORM_data8 : DW_FORM_data4)
-#define DW_FORM_ref (dwarf_offset_size == 8 ? DW_FORM_ref8 : DW_FORM_ref4)
+#define DW_FORM_data (dwarf_offset_size == 16 ? DW_FORM_data16 : (dwarf_offset_size == 8 ? DW_FORM_data8 : DW_FORM_data4))
+#define DW_FORM_ref (dwarf_offset_size == 16 ? DW_FORM_ref8 : (dwarf_offset_size == 8 ? DW_FORM_ref8 : DW_FORM_ref4))
 
 #ifndef DEBUG_FRAME_SECTION
 #define DEBUG_FRAME_SECTION	".debug_frame"
@@ -9963,6 +9963,8 @@ value_format (dw_attr_node *a)
 	  return DW_FORM_data4;
 	case 8:
 	  return DW_FORM_data8;
+	case 16:
+	  return DW_FORM_data16;
 	default:
 	  gcc_unreachable ();
 	}
@@ -9997,6 +9999,8 @@ value_format (dw_attr_node *a)
 	  return DW_FORM_data4;
 	case 8:
 	  return DW_FORM_data8;
+	case 16:
+	  return DW_FORM_data16;
 	default:
 	  gcc_unreachable ();
 	}
@@ -10035,6 +10039,10 @@ value_format (dw_attr_node *a)
 	  if (dwarf_version == 3 && a->dw_attr == DW_AT_data_member_location)
 	    return DW_FORM_udata;
 	  return DW_FORM_data8;
+	case 16:
+	  if (dwarf_version == 3 && a->dw_attr == DW_AT_data_member_location)
+	    return DW_FORM_udata;
+	  return DW_FORM_data16;
 	default:
 	  gcc_unreachable ();
 	}
@@ -10052,6 +10060,10 @@ value_format (dw_attr_node *a)
 	case 32:
 	  return DW_FORM_data8;
 	case 64:
+	  if (dwarf_version >= 5)
+	    return DW_FORM_data16;
+	  return DW_FORM_block1;
+	case 128:
 	  if (dwarf_version >= 5)
 	    return DW_FORM_data16;
 	  /* FALLTHRU */
@@ -10161,6 +10173,8 @@ value_format (dw_attr_node *a)
 	  return DW_FORM_data4;
 	case 8:
 	  return DW_FORM_data8;
+	case 16:
+	  return DW_FORM_data16;
 	default:
 	  gcc_unreachable ();
 	}
